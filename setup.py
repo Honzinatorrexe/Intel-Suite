8aeecc45ff5084017ff36ba16c2948374363a9a1
Honzinatorr <honzinatorrcls63s@gmail.com>
2026-06-15 17:47:30 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, setup(
+    name="intel-tool",
+    version="2.0",
+    author="Honzinatorrexe",
+    description="Professional Network Intelligence & Reconnaissance Toolkit",
+    install_requires=[
+        "dnspython>=2.3.0",
+        "requests>=2.31.0",
+        "scapy>=2.5.0",
+        "python-whois>=0.8.0",
+        "colorama>=0.4.6",
+    ],
+)
